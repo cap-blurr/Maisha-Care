e4d2127a91a87bc9770d315e4e0c4ed77802c6b7
anthony <kimaniantoni@gmail.com>
2024-12-14 20:51:46 +0300

    suppress hydration errors

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body suppressHydrationWarning={true} className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <PrivyProviderContainer>
           {children}
         </PrivyProviderContainer>

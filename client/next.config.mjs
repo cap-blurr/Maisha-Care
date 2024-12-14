2c9277028ee4541ec7910a8229787cfb520a7d8d
anthony <kimaniantoni@gmail.com>
2024-12-14 10:54:28 +0300

    fix: removes webpacks add email

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -2,40 +2,11 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    if (!isServer) {
-      // Fallback configurations for browser polyfills
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        crypto: 'crypto-browserify',
-        stream: 'stream-browserify',
-        buffer: 'buffer',
-        fs: false,
-        net: false,
-        tls: false,
-        path: false,
-      };
-
-      // Add plugins using the webpack instance passed as a parameter
-      config.plugins.push(
-        new webpack.ProvidePlugin({
-          Buffer: ['buffer', 'Buffer'],
-          process: 'process/browser',
-        })
-      );
-
-      config.plugins.push(
-        new webpack.NormalModuleReplacementPlugin(
-          /node:crypto/,
-          'crypto-browserify'
-        )
-      );
-    }
-
-    return config;
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
-  // Other Next.js config options
   images: {
     unoptimized: true,
   },
